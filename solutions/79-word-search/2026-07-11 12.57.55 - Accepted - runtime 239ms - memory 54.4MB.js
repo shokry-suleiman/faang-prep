33e7ac0a56250d1454e3a4b3eb2826bdbae2cc9a
shokry-suleiman <shokrysuleiman032@gmail.com>
2026-07-11 13:01:49 +0000

    Sync LeetCode solutions

---
+++ solutions/79-word-search/2026-07-11 12.57.55 - Accepted - runtime 239ms - memory 54.4MB.js
@@ -0,0 +1,35 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (isFound(board, i, j, 0, word))
+                return true
+        }
+    }
+    return false;
+};
+
+var isFound = function (board, i, j, indx, word) {
+
+    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == '#' || board[i][j] != word[indx])
+        return false
+
+    if (indx === word.length - 1)
+        return true
+
+    let char = board[i][j];
+    board[i][j] = '#';
+    if (isFound(board, i + 1, j, indx + 1, word) ||
+        isFound(board, i - 1, j, indx + 1, word) ||
+        isFound(board, i, j + 1, indx + 1, word) ||
+        isFound(board, i, j - 1, indx + 1, word)) {
+        board[i][j] = char
+        return true
+    }
+    board[i][j] = char
+    return false
+}
