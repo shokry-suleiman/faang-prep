905b631d2a7957c5c16837e1d5464b4e51cfd73a
shokry-suleiman <shokrysuleiman032@gmail.com>
2026-08-11 08:32:54 +0000

    Sync LeetCode solutions

---
+++ solutions/355-design-twitter/2026-08-11 07.50.51 - Accepted - runtime 1320ms - memory 71.5MB.js
@@ -0,0 +1,69 @@
+var node = function (userId, tweetId, next) {
+    this.userId = userId;
+    this.tweetId = tweetId;
+    this.next = next;
+}
+
+var Twitter = function () {
+    this.data = new node(0, 0, null);
+    this.map = new Map()
+    this.tail = this.data
+};
+
+/** 
+ * @param {number} userId 
+ * @param {number} tweetId
+ * @return {void}
+ */
+Twitter.prototype.postTweet = function (userId, tweetId) {
+    let newTail = new node(userId, tweetId, null)
+    this.tail.next = newTail;
+    this.tail = this.tail.next;
+};
+
+/** 
+ * @param {number} userId
+ * @return {number[]}
+ */
+Twitter.prototype.getNewsFeed = function (userId) {
+    let head = this.data.next;
+    let res = [];
+    while (head) {
+        if (head.userId == userId || this.map.get(userId)?.has(head.userId))
+            res.push(head.tweetId)
+        head = head.next
+    }
+    return res.slice(-10).reverse()
+};
+
+/** 
+ * @param {number} followerId 
+ * @param {number} followeeId
+ * @return {void}
+ */
+Twitter.prototype.follow = function (followerId, followeeId) {
+    if (!this.map.get(followerId))
+        this.map.set(followerId, new Set())
+
+    this.map.get(followerId).add(followeeId)
+};
+
+/** 
+ * @param {number} followerId 
+ * @param {number} followeeId
+ * @return {void}
+ */
+Twitter.prototype.unfollow = function (followerId, followeeId) {
+    if (!this.map.get(followerId))
+        return
+    this.map.get(followerId).delete(followeeId)
+};
+
+/** 
+ * Your Twitter object will be instantiated and called as such:
+ * var obj = new Twitter()
+ * obj.postTweet(userId,tweetId)
+ * var param_2 = obj.getNewsFeed(userId)
+ * obj.follow(followerId,followeeId)
+ * obj.unfollow(followerId,followeeId)
+ */
