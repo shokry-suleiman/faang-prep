f7488ba419f655c919cfa37a616b1797509c5477
shokry-suleiman <shokrysuleiman032@gmail.com>
2026-08-21 19:49:16 +0000

    Sync LeetCode solutions

---
+++ solutions/215-kth-largest-element-in-an-array/2026-08-21 19.42.39 - Accepted - runtime 75ms - memory 69.7MB.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+    let minHeap = new MinPriorityQueue();
+
+    for (let num of nums) {
+        minHeap.enqueue(num)
+        if (minHeap.size() > k)
+            minHeap.dequeue()
+    }
+    
+    return minHeap.front()
+};
