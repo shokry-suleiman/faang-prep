9c282d198bebc5c1fa340c91b28cc175821511a5
shokry-suleiman <shokrysuleiman032@gmail.com>
2026-09-14 12:00:30 +0000

    Sync LeetCode solutions

---
+++ solutions/1762-furthest-building-you-can-reach/2026-09-14 11.53.53 - Accepted - runtime 48ms - memory 68.6MB.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} heights
+ * @param {number} bricks
+ * @param {number} ladders
+ * @return {number}
+ */
+var furthestBuilding = function (heights, bricks, ladders) {
+    let maxHeap = new MaxPriorityQueue();
+
+    for (let i = 0; i < heights.length; i++) {
+
+        let diff = heights[i + 1] - heights[i];
+
+        if (diff <= 0)
+            continue;
+
+        bricks -= diff;
+        maxHeap.enqueue(diff);
+
+        if (bricks < 0) {
+            if (ladders == 0)
+                return i
+            ladders -= 1;
+            bricks += maxHeap.dequeue();
+        }
+
+    }
+
+    return heights.length - 1;
+};
